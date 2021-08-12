f9c210f9c17353f533d1d0ccb1c0ff126a17383b
Reut Caspi Werber <reutcaspi@gmail.com>
2021-08-13 02:12:39 +0300

    Represent data with numerical (binary) values

--- main.py
+++ main.py
@@ -37,8 +37,34 @@ def basic_plots(data):
     plt.title('Odors: Edible vs Poisonous, Partial Results')
     sns.countplot(x=new_data['odor'], hue=new_data['is-edible'], palette=['black', 'blue'], data=new_data)
     plt.ylabel('Number of Mushrooms')
-    plt.legend(title=None, labels=['Poisonous', 'Edible'])
+    plt.legend(title=None, labels=['Poisonous', 'Edible'])  # TODO - maybe this is the other way oround! check!
     plt.savefig("edible_vs_poisonous3_odor_partial.png")
+
+
+def set_data(data):
+    df = pd.DataFrame(data)
+    # set to 1 / 0 for attributes with only 2 options
+    df['is-edible'] = np.where(df['is-edible'] == 'e', 1, 0)
+    df['bruises'] = np.where(df['bruises'] == 't', 1, 0)
+    df['gill-size'] = np.where(df['gill-size'] == 'b', 1, 0)
+    df['stalk-shape'] = np.where(df['stalk-shape'] == 'e', 1, 0)
+    df['veil-type'] = np.where(df['veil-type'] == 'p', 1, 0)
+
+    # print(df.head())
+    y = df['is-edible']
+    x = df.drop(['is-edible', 'bruises', 'gill-size', 'stalk-shape',
+                 'veil-type'],
+                axis=1)
+    df2 = pd.get_dummies(x, drop_first=True)
+    df_final = pd.concat([df, df2], axis=1)
+    df_final.drop(columns=['cap-shape', 'cap-surface', 'cap-color', 'odor',
+                           'gill-color', 'stalk-surface-above-ring',
+                           'stalk-surface-below-ring', 'stalk-color-above-ring',
+                           'stalk-color-below-ring', 'veil-color', 'ring-type',
+                           'spore-print-color', 'population', 'habitat'],
+                  inplace=True)
+    df_final.to_csv('mushroom_data_binary.csv')
+    return df_final
 
 
 # get data
@@ -47,5 +73,6 @@ pd.options.display.max_rows = 100
 
 data = pd.read_csv("mushrooms_data.csv")
 # print_table(data)
-basic_plots(data)
+# basic_plots(data)
+df = set_data(data)
 
