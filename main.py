4b963f360c4d32fb17d80ed154a3ae9cd0d1331e
Reut Caspi Werber <reutcaspi@gmail.com>
2021-08-12 23:48:43 +0300

    First look at the data with plots

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-# %matplotlib inline
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()  # for plot styling
 import numpy as np
@@ -9,11 +7,45 @@ from sklearn.datasets import make_blobs
 import pandas as pd
 
 
+def print_table(data):
+    print(data)
+    print(data.shape)
+
+
+# plot edible / poisonous
+def basic_plots(data):
+    plt.figure()
+    fig1 = sns.countplot(x=data['is-edible'])
+    plt.title('Edible vs Poisonous')
+    plt.ylabel('Mushrooms')
+    fig1.set(xticklabels=['Poisonous', 'Edible'])
+    plt.savefig("edible_vs_poisonous1.png")
+
+    fig, ax = plt.subplots()
+    sns.countplot(x=data['odor'], hue=data['is-edible'], palette=['black', 'blue'], data=data)
+    plt.ylabel('Number of Mushrooms')
+    plt.legend(title=None, labels=['Poisonous', 'Edible'])
+    plt.savefig("edible_vs_poisonous2_odor.png")
+
+    new_data = data.loc[data["odor"] != "n"]
+    new_data = new_data.loc[data["odor"] != "f"]
+    # print(data["odor"].value_counts())
+    # there is only 1 value with odor m, so we will not use it.
+    new_data = new_data.loc[data["odor"] != "m"]
+
+    fig, ax = plt.subplots()
+    plt.title('Odors: Edible vs Poisonous, Partial Results')
+    sns.countplot(x=new_data['odor'], hue=new_data['is-edible'], palette=['black', 'blue'], data=new_data)
+    plt.ylabel('Number of Mushrooms')
+    plt.legend(title=None, labels=['Poisonous', 'Edible'])
+    plt.savefig("edible_vs_poisonous3_odor_partial.png")
+
+
 # get data
+pd.options.display.max_columns = 100
+pd.options.display.max_rows = 100
 
-pd.options.display.max_columns=100
-pd.options.display.max_rows=100
-data = pd.read_table("mushrooms_data.txt")
+data = pd.read_csv("mushrooms_data.csv")
+# print_table(data)
+basic_plots(data)
 
-print(data)
-print(data.shape)
